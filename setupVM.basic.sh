82cec01c0050aabcb103748c5a137ed1c1a77b77
jfosnc <jfos.nc@gmail.com>
2018-08-06 18:01:11 -0400

    Update setupVM.basic.sh

--- setupVM.basic.sh
+++ setupVM.basic.sh
@@ -12,6 +12,6 @@ mypass=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
 echo -e "$mypass\n$mypass" | passwd root
 
 #Put it in a file that gets deleted in just a bit once we get set, so we can know the passwd
-echo "$mypass" > /root/deleteME.txt
+echo "MYPASS is $mypass" > /root/VM.props
 
 
